af4f98d7937dc2b7aa4f4b68408a7f4acc7570f4
Aldis Udris <aldis@udris.lv>
2017-10-02 22:23:59 +0300

    2nd day

--- spidertest.py
+++ spidertest.py
@@ -1,12 +1,20 @@
-import scrapy
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
 
-class BlogSpider(scrapy.Spider):
-    name = 'blogspider'
-    start_urls = ['https://blog.scrapinghub.com']
+my_url = 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=100011693%204814%20600038463&IsNodeId=1&cm_sp=CAT_SSD_1-_-VisNav-_-2.5%E2%80%9D-Inch_1'
 
-    def parse(self, response):
-        for title in response.css('h2.entry-title'):
-            yield {'title': title.css('a ::text').extract_first()}
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
 
-        for next_page in response.css('div.prev-post > a'):
-            yield response.follow(next_page, self.parse)
+page_soup = soup(page_html, "html.parser")
+
+
+# parada lapas source 0_o
+# page_soup.body
+
+containers = page_soup.findAll("div",{"class":"item-container"})
+
+#parada cik atrada div ar klasi item-container
+len(containers)
