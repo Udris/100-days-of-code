573c733c3234a93c7464dbda979da640a18fce45
Aldis Udris <aldis@udris.lv>
2017-10-02 22:27:14 +0300

    saglabat iesakto

--- spidertest.py
+++ spidertest.py
@@ -1,7 +1,7 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-
+#SSD
 my_url = 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=100011693%204814%20600038463&IsNodeId=1&cm_sp=CAT_SSD_1-_-VisNav-_-2.5%E2%80%9D-Inch_1'
 
 uClient = uReq(my_url)
@@ -9,7 +9,6 @@ page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
-
 
 # parada lapas source 0_o
 # page_soup.body
